88b6c909e72f2e4d8ee59c5357e5a82ac56cc51b
Kos Great <Kos Great>
2016-10-29 14:47:50 +0300

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here.  Create metod (def) с именем post_list, который принимает в request в качестве аргумента и return (возвращает) результат работы метода render, который соберет наш шаблон страницы blog/post_list.html.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
